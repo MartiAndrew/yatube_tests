d62e9d78a9ecf1701c447d0bfb13c8c554071414
MartiAndrew <andrew.mishkov@gmail.com>
2022-12-21 01:52:58 +0300

    fourth

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django.test import Client, TestCase
 from django.urls import reverse
-from http import HTTPStatus
 from ..forms import PostForm
 from ..models import Group, Post, User
 
@@ -26,66 +25,77 @@ class PostCreateFormTests(TestCase):
             group=self.group
         )
 
+    def test_create_group_post(self):
+        """Создание поста"""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Пост группы',
+            'group': self.group.id
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        last_object = response.context['page_obj'][0]
+        self.assertRedirects(response,
+                             reverse('posts:profile',
+                                     kwargs={'username': self.user.username}
+                                     )
+                             )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertEqual(last_object.text, form_data['text'])
+        self.assertEqual(last_object.group.id, form_data['group'])
 
-def test_create_group_post(self):
-    """Создание поста группы"""
-    posts_count = Post.objects.count()
-    form_data = {
-        'text': 'Пост группы',
-        'group': self.group.id
-    }
-    response = self.authorized_client.post(
-        reverse('posts:post_create'),
-        data=form_data,
-        follow=True
-    )
-    last_object = response.context['page_obj'][0]
-    self.assertRedirects(response,
-                         reverse('posts:profile',
-                                 kwargs={'username': self.user.username}
-                                 )
-                         )
-    self.assertEqual(Post.objects.count(), posts_count + 1)
-    self.assertEqual(last_object.text, form_data['text'])
-    self.assertEqual(last_object.group.id, form_data['group'])
+    def test_edit_group_post(self):
+        """Редактирование поста"""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Текст поста с группой',
+            'group': self.group.id
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(response,
+                             reverse('posts:post_detail',
+                                     kwargs={'post_id': self.post.id}
+                                     )
+                             )
+        self.assertEqual(Post.objects.count(), posts_count)
 
+    def test_create_post_by_guest(self):
+        """Создание поста неавторизированным пользователем"""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Пытаемся создать пост',
+        }
+        response = self.client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True
+        )
+        create_url = reverse('posts:post_create')
+        login_url = reverse('users:login')
+        self.assertRedirects(response, f'{login_url}?next={create_url}')
+        self.assertEqual(Post.objects.count(), posts_count)
 
-def test_edit_group_post(self):
-    """Редактирование поста с группой"""
-    posts_count = Post.objects.count()
-    form_data = {
-        'text': 'Текст поста с группой',
-        'group': self.group.id
-    }
-    response = self.authorized_client.post(
-        reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
-        data=form_data,
-        follow=True
-    )
-    post_context = response.context['post']
-    self.assertRedirects(response,
-                         reverse('posts:post_detail',
-                                 kwargs={'post_id': self.post.id}
-                                 )
-                         )
-    self.assertEqual(Post.objects.count(), posts_count)
-    self.assertEqual(post_context.text, form_data['text'])
-    self.assertEqual(post_context.group.id, form_data['group'])
-
-
-def test_reddirect_guest_client(self):
-    '''Проверка редиректа неавторизованного пользователя'''
-    self.post = Post.objects.create(text='Тестовый текст',
-                                    author=self.user,
-                                    group=self.group
-                                    )
-    form_data = {'text': 'Текст записанный в форму'}
-    response = self.guest_client.post(
-        reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
-        data=form_data,
-        follow=True
-    )
-    self.assertEqual(response.status_code, HTTPStatus.OK)
-    self.assertRedirects(response,
-                         f'/auth/login/?next=/posts/{self.post.id}/edit/'
-                         )
+    def test_edit_post_by_guest(self):
+        """Редактирование поста неавторизированным пользователем"""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Отредактированный пост',
+        }
+        response = self.client.post(
+            reverse('posts:post_edit', kwargs={'post_id': self.post.id}),
+            data=form_data,
+            follow=True
+        )
+        edit_url = reverse('posts:post_edit', kwargs={'post_id': self.post.id})
+        login_url = reverse('users:login')
+        self.assertRedirects(response, f'{login_url}?next={edit_url}')
+        self.assertEqual(Post.objects.count(), posts_count)
+        self.assertEqual(self.post.text, self.post.text)
+        self.assertNotEqual(self.post.text, form_data['text'])
